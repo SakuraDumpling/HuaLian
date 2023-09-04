0f3c9213f914697b5e0d266aefff267d65cdc376
SakuraDumpling <1523361178@qq.com>
2023-09-05 01:07:09 +0800

    地图大半完成，需要添加敌人宝箱切换场景存档发光npc对话以及隐藏门

---
+++ Assets/脚本/BackGround.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//控制背景滚动
+public class BackGround : MonoBehaviour
+{
+    [SerializeField] private Vector2 movingSpeed;   //移动速度
+
+    private Vector2 offset;     //中转
+
+    private Material mat;   //获取这个材质
+
+    private void Awake()
+    {
+        mat = GetComponent<SpriteRenderer>().material;      //初始化
+
+    }
+
+    private void Update()
+    {
+        offset = movingSpeed * Time.deltaTime;
+        mat.mainTextureOffset += offset;        //=new Vector2(offset,0)
+    }
+}
