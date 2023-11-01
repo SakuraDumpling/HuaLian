20335ab4dbfcc5c4558b34960569ca9c090d322d
SakuraDumpling <1523361178@qq.com>
2023-11-01 10:33:46 +0800

    231101新增声音控制系统，完成到背景音乐的添加

---
+++ Assets/脚本/AudioManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;    //方便调用
+
+    public Sound[] musicSounds, sfxSounds;  //声音类名称：音乐以及效果音效
+    public AudioSource musicSource, sfxSource;
+
+    //方便调用
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayMusic("Start"); //  开始播放音乐
+    }
+
+    //播放声音的方法
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(musicSounds, x => x.name == name); //播放的音乐在音乐的组内，按需要的名字播放
+
+        //如果没有找到
+        if (s == null)
+        {
+            Debug.Log("没有找到这个音乐");
+        }
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+    }
+
+    //调用音效的方法
+    public void PlaySFX(string name)
+    {
+        Sound s = Array.Find(sfxSounds, x => x.name == name); //播放的音乐在音效的组内，按需要的名字播放
+
+        //如果没有找到
+        if (s == null)
+        {
+            Debug.Log("没有找到这个音效");
+        }
+        else
+        {
+            sfxSource.PlayOneShot(s.clip);
+        }
+    }
+}
---
+++ Assets/脚本/Sound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]   //可以方便的管理
+public class Sound 
+{
+    public string name;     //音乐的名字
+    public AudioClip clip;  //音乐
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
