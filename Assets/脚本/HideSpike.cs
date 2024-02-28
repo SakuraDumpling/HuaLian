2e00b19692ed450518ca8bdaaf1efb532ef42f0d
SakuraDumpling <1523361178@qq.com>
2024-02-28 23:41:51 +0800

    新增隐藏地刺功能，新增新手指引场景，导入对话插件

---
+++ Assets/脚本/HideSpike.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideSpike : MonoBehaviour
+{
+    public GameObject hideSpikeBox;     //地刺框
+    public float time;      //地刺时间
+
+    private Animator anim;      //获取动画
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();    //导入
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //触发的方法,其实也可以用调用的方法
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //只检测和玩家的多边形碰撞体碰撞
+        if (other.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.PolygonCollider2D")
+        {
+            //playerHealth.DamgePlayer(damge);    //调用伤害函数
+            StartCoroutine(SpikeAttack());  //启动协程
+        }
+    }
+
+    //协程
+    IEnumerator SpikeAttack()
+    {
+        yield return new WaitForSeconds(time);
+        anim.SetTrigger("Attack");      //播放动画
+        Instantiate(hideSpikeBox, transform.position, Quaternion.identity);     //让触发框使玩家受到伤害
+    }
+}
---
+++ Assets/脚本/HideSpikeBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideSpikeBox : MonoBehaviour
+{
+    public int damage;  //伤害值
+    public float destroyTime;   //消失时间
+
+    private PlayerHealth playerHealth;  //获取玩家生命
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>(); //初始化玩家生命
+        Destroy(gameObject, destroyTime);   //碰撞框消失
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //触发的方法
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //只检测和玩家的多边形碰撞体碰撞
+        if (other.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.PolygonCollider2D")
+        {
+            playerHealth.DamgePlayer(damage);    //调用伤害函数
+        }
+    }
+}
