d33e3800746c9de7b8923d1469f666ff055c8862
SakuraDumpling <1523361178@qq.com>
2024-02-29 03:34:38 +0800

    对话系统完成

---
+++ Assets/脚本/TreasureDialogue.cs
@@ -0,0 +1,29 @@
+﻿using NodeCanvas.DialogueTrees;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureDialogue : MonoBehaviour
+{
+    DialogueTreeController dialogueTree;    //获取对话树
+
+    private void Start()
+    {
+        dialogueTree = GetComponent<DialogueTreeController>();  //初始化
+    }
+
+    void Update()
+    {
+        //如果获取到的按键为I
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            dialogueTree.StartDialogue();   //调取打开对话
+        }
+    }
+
+    //触发对话
+    public void Talk()
+    {
+        dialogueTree.StartDialogue();   //调取打开对话
+    }
+}
