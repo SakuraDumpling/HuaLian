96ef2ae4a4fe57550fd101530011c6dedd8af759
SakuraDumpling <1523361178@qq.com>
2024-03-20 17:18:48 +0800

    动画分离调用成功，但多个动画会出现干扰

---
+++ Assets/脚本/AnimController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimController : MonoBehaviour
+{
+
+    public float moveUnit;  //移动位置
+    public float appearTime;  //出现时间
+    public RectTransform rectTransform;//坐标组件
+    public Image img;   //图像
+
+    public GameObject imgTransform;     //存储位置
+    public float floatUnit;             //浮动单位
+    public float timFloat;              //浮动时间
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //animenter = new AnimEnter();    //初始化,可以运行，但是有黄色警告
+        //animenter = GetComponent<AnimEnter>();      //获取脚本组件
+        AnimEnter.PositionMoveX(moveUnit, rectTransform);
+        AnimEnter.FadeIn(appearTime, img);
+
+        //MainMenuFloat.Float(floatUnit, timFloat, imgTransform);     //浮动代码
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+}
--- Assets/脚本/MainMenuFloat.cs
+++ Assets/脚本/MainMenuFloat.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 
 public class MainMenuFloat : MonoBehaviour
 {
+
     //浮动
-    Vector2 upFloat;    //上顶点
-    Vector2 downFloat;  //下顶点
-    [SerializeField]
-    float duration = 10f;  //浮动5个单位
-    [SerializeField]
-    float timFloat = 1.2f;  
+    private static Vector2 upFloat;    //上顶点
+    private static Vector2 downFloat;  //下顶点
+    public float floatUnit = 10f;  //浮动5个单位
+    public float timFloat = 1.2f;
+    public GameObject imgTransform;     //存储位置
 
     // Start is called before the first frame update
     void Start()
     {
         //测试用
-        Float();
+        //Float();
     }
 
     // Update is called once per frame
@@ -26,13 +26,13 @@ public class MainMenuFloat : MonoBehaviour
         
     }
 
-    public void Float()
+    public static void Float(float floatUnit, float timFloat, GameObject imgTransform)
     {
         //初始设为            （原始的x，原始y+浮动的数值）
-        upFloat = new Vector2(transform.localPosition.x, transform.localPosition.y + duration);
-        downFloat = new Vector2(transform.localPosition.x, transform.localPosition.y - duration);
+        upFloat = new Vector2(imgTransform.transform.localPosition.x, imgTransform.transform.localPosition.y + floatUnit);
+        downFloat = new Vector2(imgTransform.transform.localPosition.x, imgTransform.transform.localPosition.y - floatUnit);
 
         //缓动
-        DOTween.To(() => upFloat, x => transform.localPosition = x, downFloat, timFloat).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+        DOTween.To(() => upFloat, x => imgTransform.transform.localPosition = x, downFloat, timFloat).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
     }
 }
