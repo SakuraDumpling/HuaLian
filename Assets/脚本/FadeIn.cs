a919961309477776c3180e007bf42d760dd977e5
SakuraDumpling <1523361178@qq.com>
2024-03-26 09:35:40 +0800

    插件重装前的备份

---
+++ Assets/脚本/FadeIn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.AnimatedValues;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class FadeIn : MonoBehaviour
+{
+    public float appearTime;  //出现时间
+    public UnityEngine.UI.Image img;   //图像
+
+    public GameObject imgTransform;     //存储位置
+    public float floatUnit;             //浮动单位
+    public float timFloat;              //浮动时间
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnimEnter.FadeIn(appearTime, img);
+        MainMenuFloat.Float(floatUnit, timFloat, imgTransform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
